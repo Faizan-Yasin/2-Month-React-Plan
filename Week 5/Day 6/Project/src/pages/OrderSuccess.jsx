eb36187ffd301d51255bccd185c704973a2c346e
Faizan-Yasin <faizanyasin682@gmail.com>
2026-07-22 19:28:57 +0500

    Week 5 Day 6 Completed

--- Week 5/Day 6/Project/src/pages/OrderSuccess.jsx
+++ Week 5/Day 6/Project/src/pages/OrderSuccess.jsx
@@ -15,11 +15,23 @@ const OrderSuccess = () => {
       transition={{ duration: 0.4 }}
       className=" flex flex-col items-center justify-center h-screen">
 
-      <motion.h1 className=" text-4xl font-bold text-green-600">Order Confirmed</motion.h1>
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className=" text-4xl font-bold text-green-600">Order Confirmed</motion.h1>
 
-      <motion.p className="mt-4">Thank you for your purchase.</motion.p>
+      <motion.p
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className="mt-4">Thank you for your purchase.</motion.p>
 
-      <motion.Link onClick={() => dispatch(clearCart())} to="/" className=" mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg">
+      <motion.Link
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        onClick={() => dispatch(clearCart())} to="/" className=" mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg">
         Continue Shopping
       </motion.Link>
 
